0fc5a79d0ff58040ebd766110f3d7884484dee42
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-10-30 17:51:43 -0400

    up to forms

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        
+        @dog = Dog.find(params[:id])
+        @employees = Employee.all.select {|employee| employee.dog == params[:id].to_i}
+       #byebug
+    end
+
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
+
 class Employee < ApplicationRecord
+    def name
+        [self.first_name, " ", self.last_name].join(" ") 
+    end
+
 end
